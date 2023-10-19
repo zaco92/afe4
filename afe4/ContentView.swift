87eb6984b8a2c5e7f3649971a3367e0a6e4721c6
zaco92 <zachariwahib@gmail.com>
2023-10-19 16:24:58 +0200

    Initial Commit

---
+++ afe4/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  afe4
+//
+//  Created by wahib zachari on 19/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ZStack{
+                Rectangle()         
+                    .cornerRadius(20)
+                    .frame(width:350, height: 180)
+                    .foregroundColor(Color("greenStudent"))
+                Text("numero utile")}
+            ZStack{
+            
+            Rectangle()         
+                    .cornerRadius(20)
+                    .frame(width:350, height: 180)
+            
+                    .foregroundColor(Color("greenStudent"))
+                HStack{
+                    VStack{
+                        Text("Harcelement") ;
+                        Text("0909090909")
+                       
+                        
+                    }
+                    Image(systemName:"phone")
+                    
+                }
+            }
+                            
+                        
+                
+        }
+            Spacer()
+        
+        }
+        
+      
+
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ afe4/afe4App.swift
@@ -0,0 +1,17 @@
+//
+//  afe4App.swift
+//  afe4
+//
+//  Created by wahib zachari on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct afe4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
