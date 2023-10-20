6aced26d9d239506a3ebcee9bfd18250b61df8d8
zaco92 <zachariwahib@gmail.com>
2023-10-20 17:12:27 +0200

    docu eleve non fonctionel

---
+++ afe4/docEleve.swift
@@ -0,0 +1,18 @@
+//
+//  docEleve.swift
+//  afe4
+//
+//  Created by wahib zachari on 20/10/2023.
+//
+
+import SwiftUI
+
+struct docEleve: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    docEleve()
+}
