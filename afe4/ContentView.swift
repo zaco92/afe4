2242ca5e17ae485cf7eb6f401f565060613450c0
zaco92 <zachariwahib@gmail.com>
2023-10-20 14:51:31 +0200

    list

--- afe4/ContentView.swift
+++ afe4/ContentView.swift
@@ -9,47 +9,126 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    
         VStack {
+            HStack{
+                Spacer()
+                Image(systemName:"person.crop.circle.fill.badge.minus")
+                    .font(.system(size: 50, weight: .thin))
+            }
+            
             ZStack{
-                Rectangle()         
+                Rectangle()
                     .cornerRadius(50)
-                    .frame(width:350, height: 180)
+                    .frame(width:350, height: 100)
+                    .padding()
                     .foregroundColor(Color("greenStudent"))
-                Text("numero IMPORTANT")}
-            ZStack{
-            
-            Rectangle()         
-                    .cornerRadius(20)
-                    .frame(width:350, height: 180)
-            
-                    .foregroundColor(Color("greenStudent"))
-                HStack{
+                Text("CONTACTS IMPORTANT")
+            }
+            List{
+                VStack{
+                    ZStack{
+                        
+                        Rectangle()
+                            .cornerRadius(20)
+                            .frame(width:350, height: 100)
+                        
+                            .foregroundColor(Color.gray)
+                        HStack{
+                            VStack{
+                                Text("medecin") ;
+                                Text("0909090909")
+                                
+                                
+                            }
+                            .padding(20)
+                            Image("phone-call")
+                                .resizable()
+                                .frame(width:30,height: 30)
+                                .background(
+                                    Circle()
+                                        .foregroundColor(.green)
+                                    
+                                        .frame(width:70, height:70)                            )
+                            
+                        }
+                    }
                     VStack{
-                        Text("medecin") ;
-                        Text("0909090909")
-                       
+                        
+                        
+                        ZStack{
+                            
+                            Rectangle()
+                                .cornerRadius(20)
+                                .frame(width:350, height: 100)
+                            
+                                .foregroundColor(Color.gray)
+                            HStack{
+                                VStack{
+                                    Text("infirmerie") ;
+                                    Text("0909090909")
+                                    
+                                    
+                                }
+                                .padding(20);                            Image("phone-call")
+                                .resizable()
+                                .frame(width:30,height: 30)
+                                .background(
+                                    Circle()
+                                        .foregroundColor(.green)
+                                        .frame(width:70, height:70)                                   )
+                                
+                                
+                                
+                                
+                            }
+                        }
+                        
+                        VStack{
+                            
+                            
+                            ZStack{
+                                
+                                Rectangle()
+                                    .cornerRadius(20)
+                                    .frame(width:350, height: 100)
+                                
+                                    .foregroundColor(Color.gray)
+                                HStack{
+                                    VStack{
+                                        Text("harcelement") ;
+                                        Text("0909090909")
+                                        
+                                        
+                                    }
+                                    .padding(20);                                 Image("phone-call")
+                                    .resizable()
+                                    .frame(width:30,height: 30)
+                                    .background(
+                                        Circle()
+                                            .foregroundColor(.green)
+                                            .frame(width:70, height:70)                                       )
+                                    
+                                }
+                            }}
+                        
+                        
                         
                     }
-                    Image(systemName:"phone")
-                    
                 }
-            }
-                            
-                        
+                Spacer()
                 
+            }
+            
         }
-            Spacer()
         
-        }
-        
-      
-
         
     }
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
