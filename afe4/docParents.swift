630b13427a6c99d82b837be4592fcab1e39e7652
zaco92 <zachariwahib@gmail.com>
2023-10-24 16:01:59 +0200

    code zac ajout doc parent

---
+++ afe4/docParents.swift
@@ -0,0 +1,18 @@
+//
+//  docParents.swift
+//  afe4
+//
+//  Created by wahib zachari on 24/10/2023.
+//
+
+import SwiftUI
+
+struct docParents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    docParents()
+}
